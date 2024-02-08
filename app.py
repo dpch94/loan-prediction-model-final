12942fe7b250e2471350c4b11ff6f47f3266371f
Devi Prasad Ilapavuluri <dpch94@gmail.com>
2024-02-08 09:02:53 +0100

    feat:reverted to old template and model due to deployment restrictions

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 #load the saved model
 def load_model():
-    return pickle.load(open('histgrad_lpred_model.pkl', 'rb'))
+    return pickle.load(open('loanpred_final_model.pkl', 'rb'))
 
 #home page
 @app.route('/')
@@ -23,38 +23,21 @@ def predict():
     '''
     For rendering results on HTML
     '''
-    data_in = None
-    if request.method == 'POST':
+   
+    labels = ['not granted','granted']
 
-        data_in = request.form.to_dict()      
+    features = [float(x) for x in request.form.values()]
 
-        labels = ['not granted','granted']
-        pred_df = pd.DataFrame([data_in.values()], columns=data_in.keys())
-        print(pred_df)
+    values = [np.array(features)]
 
-        pred_df['Gender'] = pred_df['Gender'].astype('category')
-        pred_df['Married'] = pred_df['Married'].astype('category')
-        pred_df['Dependents'] = pred_df['Dependents'].astype('category')
-        pred_df['Education'] = pred_df['Education'].astype('category')
-        pred_df['Self_Employed'] = pred_df['Self_Employed'].astype('category')
-        pred_df['Property_Area'] = pred_df['Property_Area'].astype('category')
-        
-        pred_df['ApplicantIncome'] = pred_df['ApplicantIncome'].astype('float64')
-        pred_df['CoapplicantIncome'] = pred_df['CoapplicantIncome'].astype('float64')
-        pred_df['LoanAmount'] = pred_df['LoanAmount'].astype('float64')
-        pred_df['Loan_Amount_Term'] = pred_df['Loan_Amount_Term'].astype('float64')
-        pred_df['Credit_History'] = pred_df['Credit_History'].astype('float64')
+    model = load_model()
 
-        model = load_model()
-        prediction = model.predict(pred_df)
+    prediction = model.predict(values)
 
-        result = labels[prediction[0]]
+    result = labels[prediction[0]]
 
-        return render_template('index.html', output='The Loan is {}'.format(result)) 
-
-
-    else:
-        return "Error. Please try again later."
+    return render_template('index.html', output='The Loan is {}'.format(result))
+   
 
 if __name__ == "__main__":
     port=int(os.environ.get('PORT',5000))
